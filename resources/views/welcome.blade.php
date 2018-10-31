70552dfc398bca6e48f37c9b1e492cdb702aab4b
phlimasg <raphaelpc_@hotmail.com>
2018-10-31 18:52:53 -0300

    Laravel -> Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Commit
                 </div>
 
                 <div class="links">
