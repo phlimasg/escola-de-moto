6ca8626cb5aac7737a3a6b0604ef38dbc19036e8
phlimasg <raphaelpc_@hotmail.com>
2018-11-30 18:23:45 -0200

    PaginaInicial
    Adicionado alguns elementos ao index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,130 @@
+@extends('layouts.site')
+@section('content')
+
+
+<!--Início do Slide-->    
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>          
+        </ol>
+    
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+    
+          <div class="item active">
+          <img src="{{asset('img/banner-1.jpg')}}" alt="Los Angeles" style="width:100%;">
+            <div class="carousel-caption">
+              <h3>Aulas 100% Práticas</h3>
+              <p>Faça uma aula gratuita!</p>
+              <button class="btn btn-quero">Tenho interesse!</button>
+            </div>
+          </div>
+    
+          <div class="item">
+            <img src="{{asset('img/banner-2.jpg')}}" alt="Chicago" style="width:100%;">
+            <div class="carousel-caption">
+                <h3>Aulas 100% Práticas</h3>
+                <p>Faça uma aula gratuita!</p>
+                <button class="btn btn-quero">Tenho interesse!</button>
+            </div>
+          </div>
+      
+        </div>
+    
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div> 
+<!--Fim do Slide-->   
+<!--Cursos-->   
+<div class="container-fluid">
+  <h3><span class="glyphicon glyphicon-education"></span> Nossos Cursos</h3>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+          <div class="thumbnail">
+            
+              <img src="/img/lights.jpg" alt="Lights" style="width:100%">
+              <div class="caption">
+                <h3>Módulo 1</h3>
+                <p>Lorem ipsum donec id elit non mi porta gravida at eget metus. <a href="">Saiba Mais!</a>  </p>
+                <button class="btn btn-danger">Possuo Interesse</button>
+              </div>
+            
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="thumbnail">
+              <img src="/img/fjords.jpg" alt="Lights" style="width:100%">
+              <div class="caption">
+                <h3>Módulo 2</h3>
+                <p>Lorem ipsum donec id elit non mi porta gravida at eget metus. <a href="">Saiba Mais!</a>  </p>
+                <button class="btn btn-danger">Possuo Interesse</button>
+              </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="thumbnail">
+              <img src="/img/nature.jpg" alt="Lights" style="width:100%">
+              <div class="caption">
+                <h3>Módulo 3</h3>
+                <p>Lorem ipsum donec id elit non mi porta gravida at eget metus. <a href="">Saiba Mais!</a>  </p>
+                <button class="btn btn-danger">Possuo Interesse</button>
+              </div>
+          </div>
+        </div>
+      </div>
+</div>
+</div>
+<!--Fim Cursos-->   
+
+<!--Contato-->   
+<div class="container-fluid">
+  <h3><span class="glyphicon glyphicon-earphone"></span>  Contato</h3>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <label for="name">Nome:</label>
+        <input type="text" class="form-control">
+          <div class="row">
+            <div class="col-sm-7">
+                <label for="mail">E-mail:</label>
+                <input type="mail" class="form-control">
+            </div>        
+            <div class="col-sm-5">
+                <label for="tel">Telefone</label>
+                <input type="tel" class="form-control">
+            </div>
+            <div class="col-sm-12">
+                <label for="contato">Descreva seu contato:</label>
+                <textarea name="contato" id=""  rows="10" class="form-control"></textarea>
+                <hr>
+                <button class="btn btn-danger btn-lg">Enviar contato</button>
+              </div>            
+          </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!--Fim Contato-->   
+
+<!--Maps-->   
+<div class="" name="map">
+  <div class="container-fluid">
+    <h3><span class="glyphicon glyphicon-map-marker"></span> Onde Estamos</h3>
+  </div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.6837003072587!2d-43.12153078563515!3d-22.88813988502132!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9983c084313dd1%3A0xa50b27d8ce6abe9e!2sR.+Visc.+de+Sepetiba%2C+105+-+Centro%2C+Niter%C3%B3i+-+RJ%2C+24020-206!5e0!3m2!1spt-BR!2sbr!4v1543606787363" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+<!--Fim Maps--> 
+
+
+@endsection
---
+++ resources/views/layouts/site.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>  
+  <title>{{env('APP_NAME')}}</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<body>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span> 
+            </button>
+            <a class="navbar-brand" href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Logo_TV_2015.png" class="logo" alt="" ></a>
+          </div>
+          <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Início</a></li>
+              <li><a href="#">Cursos</a></li>
+              <li><a href="#">Manuais Técnicos</a></li> 
+              <li><a href="#">Contato</a></li> 
+              <li><a href="#">Onde estamos</a></li> 
+              <li><a href="#">Sobre a Escola</a></li>
+            </ul>
+            <!--<ul class="nav navbar-nav navbar-right">              
+              <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>-->
+          </div>
+        </div>
+    </nav>
+    @yield('content')
+    <div class="footer">
+        <div align="center">
+            <br><br><br>
+          <span>
+              Mj Soares Cursos Profissionalizantes
+          </span>          
+          <br><br><br><br>
+            
+        </div>
+      </div>
+</body>
+</html>
